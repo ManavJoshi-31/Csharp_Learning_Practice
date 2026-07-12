c4e21830e8c73994fd52996f4368894d6efc0c06
ManavJoshi-31 <manavjoshi313@gmail.com>
2026-07-12 20:09:38 +0530

    C# projects

---
+++ Guess_the_Number/Program.cs
@@ -0,0 +1,25 @@
+﻿int predefined = 98;
+int x;
+int attempts = 0;
+
+while(true)
+{
+    Console.Write("Make your guess : ");
+    x = Convert.ToInt32(Console.ReadLine());
+    attempts++;
+
+    if (x > predefined)
+    {
+        Console.WriteLine("Guess Lower");
+    }
+    else if (x < predefined)
+    {
+        Console.WriteLine("Guess Higher");
+    }
+    else
+    {
+        break;
+    }
+}
+ Console.WriteLine("Congratulations! you got that!");
+ Console.WriteLine("You guessed the number in " + attempts   + " attempts");
